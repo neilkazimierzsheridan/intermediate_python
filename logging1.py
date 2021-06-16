c228199230121c4caac90418b48e571fb119a613
neilkazimierzsheridan <65231357+neilkazimierzsheridan@users.noreply.github.com>
2021-06-16 22:08:10 +0100

    Add logging 1

---
+++ logging1.py
@@ -0,0 +1,17 @@
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG) #see the docs
+#level at debug to show all messages
+
+import helper
+
+
+
+logging.debug('This is  debug message')
+logging.info('This is info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
+
+
